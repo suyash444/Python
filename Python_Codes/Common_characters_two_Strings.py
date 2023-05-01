d189d8293f830dc00b7d7fe5e15e0be51281bf39
Suyash Singh <singhsuyash444@gmail.com>
2023-05-01 12:20:22 +0200

    Strings

--- Python_Codes/Common_characters_two_Strings.py
+++ Python_Codes/Common_characters_two_Strings.py
@@ -5,9 +5,25 @@ def func():
     x1 = set(a)
     x2 = set(b)
 
-    result = x1 and x2
+    result = x1 & x2
 
     print("the common characters in both the string are:", result)
 
 
 func()
+
+# using for loop find the common characters
+
+a1 = input("Enter the first string:")
+a2 = input("Enter the second string:")
+
+common_char = ""
+
+for char1 in a1:
+    for char2 in a2:
+        if char1 == char2:
+            common_char += char1
+
+print("common_characters:", common_char)
+
+
